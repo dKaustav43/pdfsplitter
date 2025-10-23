0bb6fd3a62a2254ca8effe37ce1d4ee85d157712
Kaustav Dey <dkaustav43@gmail.com>
2025-10-23 16:37:39 +0100

    Modified code

--- splittingpdfs.py
+++ splittingpdfs.py
@@ -9,19 +9,28 @@ if not os.path.isfile(file_name):
 else:
     inputpdf = PdfReader(open(file_name, "rb"))
 
-    for i in range(0, 6):
-        writer = PdfWriter()
+    # for i in range(0, 6):
+    #     writer = PdfWriter()
+    #     writer.add_page(inputpdf.pages[i])
+    #     with open("%s-page%s.pdf" % (file_name, i), "wb") as output_pdf1:
+    #         writer.write(output_pdf1)
+
+    # # Merge the split PDF files
+    # merge_pdf = PdfMerger()
+
+    # for i in range(0, 6):
+    #     merge_pdf.append(open("%s-page%s.pdf" % (file_name, i), "rb"))
+
+    # with open("merged_%s" % file_name, "wb") as output_pdf1:
+    #     merge_pdf.write(output_pdf1)
+    writer = PdfWriter()
+
+    for i in range(min(6,len(inputpdf.pages))):
         writer.add_page(inputpdf.pages[i])
-        with open("%s-page%s.pdf" % (file_name, i), "wb") as output_pdf1:
-            writer.write(output_pdf1)
 
-    # Merge the split PDF files
-    merge_pdf = PdfMerger()
+    # Write to a single merged PDF
+    with open("merged.pdf", "wb") as output_pdf:
+        writer.write(output_pdf)
 
-    for i in range(0, 6):
-        merge_pdf.append(open("%s-page%s.pdf" % (file_name, i), "rb"))
-
-    with open("merged_%s" % file_name, "wb") as output_pdf1:
-        merge_pdf.write(output_pdf1)
-
+    print(f"Created '{"merged.pdf"}' with the first six pages.")
 # %%
