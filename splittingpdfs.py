9bba425ee0a9b872ad916906227d16d5372e5021
Kaustav Dey <dkaustav43@gmail.com>
2025-10-23 16:44:48 +0100

    Code to split pdf in a partcilar range of pages

--- splittingpdfs.py
+++ splittingpdfs.py
@@ -1,36 +1,21 @@
-#%%
 import os
 from PyPDF2 import PdfWriter, PdfReader, PdfMerger
-#%%
+
 file_name = "sample.pdf"
-#%%
+
 if not os.path.isfile(file_name):
     print(f"The file {file_name} does not exist.")
 else:
     inputpdf = PdfReader(open(file_name, "rb"))
 
-    # for i in range(0, 6):
-    #     writer = PdfWriter()
-    #     writer.add_page(inputpdf.pages[i])
-    #     with open("%s-page%s.pdf" % (file_name, i), "wb") as output_pdf1:
-    #         writer.write(output_pdf1)
-
-    # # Merge the split PDF files
-    # merge_pdf = PdfMerger()
-
-    # for i in range(0, 6):
-    #     merge_pdf.append(open("%s-page%s.pdf" % (file_name, i), "rb"))
-
-    # with open("merged_%s" % file_name, "wb") as output_pdf1:
-    #     merge_pdf.write(output_pdf1)
     writer = PdfWriter()
 
-    for i in range(min(6,len(inputpdf.pages))):
+    # for i in range(min(6,len(inputpdf.pages))):
+    for i in range(100,200):
         writer.add_page(inputpdf.pages[i])
 
     # Write to a single merged PDF
     with open("merged.pdf", "wb") as output_pdf:
         writer.write(output_pdf)
 
-    print(f"Created '{"merged.pdf"}' with the first six pages.")
-# %%
+    print(f"Created '{"merged.pdf"}'")
