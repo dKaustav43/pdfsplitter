9e80f24db1c92ac7983f08ca8aca48154d461ce2
dKaustav43 <dkaustav43@gmail.com>
2026-01-06 00:24:26 +0000

    Refactoring the structure again

--- splittingpdfs.py
+++ splittingpdfs.py
@@ -1,36 +1,30 @@
-
 from pathlib import Path
 from PyPDF2 import PdfWriter, PdfReader
 
-
-def check_input_pdfpath(input_pdf_path:str):
-    
-    input_path = Path(input_pdf_path)    
-    if not input_path.is_file():
+def check_input_pdfpath(input_pdf_path:str) -> Path:
+    """(I/O)Checks and returns a path object from an input_pdf_path as a string input."""
+    path = Path(input_pdf_path)    
+    if not path.is_file():
         raise FileNotFoundError(f"Input file does not exist: {input_pdf_path}")
     
-    return input_path
+    return path
     
-def read_and_validate_pdf(input_pdf_path:str, start_page:int, end_page:int):
-    
-    input_path = check_input_pdfpath(input_pdf_path)
+def read_and_validate_pdf(input_pdf_path:str) -> PdfReader:
+    """(Helper function)Reads the pdf file and returns an object of type PdfReader."""
+    path = check_input_pdfpath(input_pdf_path)
 
-    reader = PdfReader(str(input_path))
+    reader = PdfReader(str(path))
     
+    return reader
+
+def write_to_newpdf(reader:PdfReader, start_page:int, end_page:int) -> PdfWriter:
+    """Uses the Reader helper function and store custom pdf pages to a PdfWriter Object"""
     total_pages = len(reader.pages)
-
+    
     # validate pages
     if start_page < 0 or end_page >= total_pages:
         raise ValueError("Page range is out of bounds."
                          f"PDF has {total_pages} pages.")
-
-    return reader
-
-def write_to_newpdf(input_pdf_path:str, start_page:int, end_page:int):
-    
-    input_path = check_input_pdfpath(input_pdf_path)
-    
-    reader = PdfReader(input_path)
     
     writer = PdfWriter()
     # add pages to the writer
@@ -39,63 +33,28 @@ def write_to_newpdf(input_pdf_path:str, start_page:int, end_page:int):
     
     return writer
 
-def output_split_pdf(input_pdf_path:str, output_pdf_path:str, start_page:int,end_page:int):
-    
+def output_split_pdf(writer:PdfWriter, output_pdf_path:str) -> str:
+    """(I/O)Uses the write helper function and returns a pdf in output_pdf_path location."""
     # Write to a single merged PDF
     output_path = Path(output_pdf_path)
-    writer = write_to_newpdf(input_pdf_path, start_page,end_page)
+    
     with open(output_path, "wb") as output_pdf:
         writer.write(output_pdf)
     
     return str(output_path)
 
-# def split_pdf(input_pdf_path:str, output_pdf_path:str, start:int, end:int):
-
-#     """
-#     Takes a PDF and extracts pages from start to end (inclusive),
-#     writing them into a new PDF located at the output_pdf_path.
-#     """
-
-#     input_path = Path(input_pdf_path)
-
-#     if not input_path.is_file():
-#         raise FileNotFoundError(f"Input file does not exist: {input_pdf_path}")
-
-#     reader = PdfReader(str(input_path))
-#     total_pages = len(reader.pages)
-
-#     # validate pages
-#     if start < 0 or end >= total_pages:
-#         raise ValueError("Page range is out of bounds."
-#                          f"PDF has {total_pages} pages.")
-
-#     writer = PdfWriter()
-
-#     # add pages to the writer
-#     for i in range(start, end+1):
-#         writer.add_page(reader.pages[i])
-    
-#     # Write to a single merged PDF
-#     output_path = Path(output_pdf_path)
-#     with open(output_path, "wb") as output_pdf:
-#         writer.write(output_pdf)
-    
-#     return str(output_path)
-
 def main():
 
     input_pdf_path = input("Enter the path of the pdf file here: ")
-    
-    
     output_pdf_path = input("Enter the path where you would like to store the split pdf: ")
 
     start_page = int(input("Start page index: "))
     end_page = int(input("End page inded: "))
 
-    validate_page_range = read_and_validate_pdf(input_pdf_path,start_page,end_page)
-    print(validate_page_range)
+    reader = read_and_validate_pdf(input_pdf_path)
+    writer = write_to_newpdf(reader,start_page,end_page)
 
-    result = output_split_pdf(input_pdf_path, output_pdf_path, start_page, end_page)
+    result = output_split_pdf(writer,output_pdf_path)
 
     print(f"Created '{result}'!")
 
