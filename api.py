09d0c5f2c9a10b60fda64d618e6741531136cc8d
dKaustav43 <dkaustav43@gmail.com>
2026-09-22 17:48:14 +0100

    set the background task for deleting file

--- api.py
+++ api.py
@@ -4,19 +4,26 @@
 ## and deletes the output when the split pdf is sent back.
 
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 
 app = FastAPI()
 
+async def delete_file(uploaded_file:UploadFile):
+    await uploaded_file.close()
+
 @app.post("/upload/")
-async def upload_file(file: UploadFile | None = None):
+async def upload_file(background_task : BackgroundTasks,
+                      file: UploadFile | None = None):
     if not file:
         return {"message":"No file sent"}
     else:
         contents = await file.read()
+        background_task.add_task(delete_file, file)
         return {
             "filename": file.filename,
             "content_type": file.content_type,
-            "file size": f"{len(contents)/1000} mb"
+            "file size": f"{len(contents)/1000} mb",
+            "message" : "file deleted from memory in the background"
             }
+        
 
